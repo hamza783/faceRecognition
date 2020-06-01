01459d01df935b7e0f8e552b89899b42fd4d5840
Hamza <Hamza4253@gmail.com>
2020-05-31 20:48:59 -0500

    Updates

---
+++ precache-manifest.57ef36997bc9239b285c486372439f37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f1dc8fc86e21ee4236503acadefa76",
+    "url": "/faceRecognition/index.html"
+  },
+  {
+    "revision": "664b8039afe8d84f2afc",
+    "url": "/faceRecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7b800ac7301c5567aeb9",
+    "url": "/faceRecognition/static/css/main.e08baecc.chunk.css"
+  },
+  {
+    "revision": "664b8039afe8d84f2afc",
+    "url": "/faceRecognition/static/js/2.e1184413.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/faceRecognition/static/js/2.e1184413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b800ac7301c5567aeb9",
+    "url": "/faceRecognition/static/js/main.f900e88e.chunk.js"
+  },
+  {
+    "revision": "ee6a8fe36dcaee905098",
+    "url": "/faceRecognition/static/js/runtime-main.4ab92352.js"
+  }
+]);
